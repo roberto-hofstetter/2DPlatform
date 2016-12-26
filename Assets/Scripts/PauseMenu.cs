04bd7d45cd7e053846872e8919ba2667cdc08b77
Roberto Hofstetter <roberto.rhd@gmail.com>
2016-12-26 21:20:42 -0200

    camera follow, bounds and menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject PanelUI;
+
+    private bool paused = false;
+
+    void Start()
+    {
+        PanelUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;            
+        }
+
+        if (paused)
+        {
+            PanelUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)
+        {
+            PanelUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,64 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-    public float MaxSpeed = 3;
+    public float MaxSpeed = 6;
     public float Speed = 50f;
     public float JumpPower = 150f;
 
     public bool Grounded;
+    public bool CanDoubleJump;
 
     private Rigidbody2D Rbd2d;
     private Animator anim;
 
+    public int ourHealth;
+    public int maxHealth; 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         Rbd2d = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        anim.SetBool("Grounded",Grounded);
-        anim.SetFloat("Speed", Mathf.Abs(Rbd2d.velocity.x));
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Grounded", Grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
 
 
         //flip the player 
-        if(Input.GetAxis("Horizontal") < -0.1f)
+        if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
         }
 
-        if (Input.GetAxis("Horizontal") > 0.1f)
+        if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
         }
 
-        if (Input.GetButtonDown("Jump") && Grounded)
+        if (Input.GetButtonDown("Jump"))
         {
-            Rbd2d.AddForce(Vector2.up * JumpPower);
-        }
-    }
-
-    void FixedUpdate()
-    {
-
-        //moving the player
-        float h = Input.GetAxis("Horizontal");                
-
-        Rbd2d.AddForce((Vector2.right * Speed) * h);
-
-        // limiting the speed of the player
-        if (Rbd2d.velocity.x > MaxSpeed)
-        {
-            Rbd2d.velocity = new Vector2(MaxSpeed, Rbd2d.velocity.y);
-        }
-
-        if(Rbd2d.velocity.x < -MaxSpeed)
-        {
-            Rbd2d.velocity = new Vector2(-MaxSpeed, Rbd2d.velocity.y);
+            if (Grounded)
+            {
+                Rbd2d.AddForce(transform.up * JumpPower);
+                CanDoubleJump = true;
+            }else
+            {
+                if (CanDoubleJump)
+                {
+                    CanDoubleJump = false;
+                    Rbd2d.velocity = new Vector2(Rbd2d.velocity.x, 0);
+                    Rbd2d.AddForce(Vector2.up * JumpPower / 1.75f);
+                }
+            }
+            
         }
     }
 }
